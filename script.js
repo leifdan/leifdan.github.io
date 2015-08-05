9bf85333d885ce7d7ad74f38081152e7c9c4aa8c
leifdan01 <leifdan@gmail.com>
2015-08-04 22:04:01 -0400

    display number of inning option

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function doSomethingElse() {
   alert("something else");
 }
+
+function onload() {
+  //populate inning dropdown
+  var inningSelector = document.getElementById('innings');
+  for (var index = 7; index <= 20; index++){
+     var opt = document.createElement("option");
+     opt.value= index;
+     opt.innerHTML = index; // whatever property it has
+
+     // then append it to the select element
+     inningSelector.appendChild(opt);
+  }  
+}
